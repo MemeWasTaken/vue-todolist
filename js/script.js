1ab49a96d99dbef8928741ccdb9a10187bba7b47
MemeWasTaken <memelazzari02@gmail.com>
2021-12-17 13:06:47 +0100

    layout base

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
-const app = new Vue(
-    {
-        el: '#containerVue',
-        data: {
-
+const app = new Vue({
+    el: '#containerVue',
+    data: {
+      tasks: [
+        {
+          textToDo: 'Lorem ipsum dolor sit amet consectetur adipisicing',
+          completed: false,
         },
-        methods: {
-            
-        }
+        {
+          textToDo: 'Lorem  adipisicing asda',
+          completed: false,
+        },
+        {
+          textToDo: 'Lorem adipisicing',
+          completed: false,
+        },
+        {
+          textToDo: 'Sit amet consectetur adipisicing',
+          completed: false,
+        },
+      ]
+    },
+    methods: {
+
     }
-)
+  });
