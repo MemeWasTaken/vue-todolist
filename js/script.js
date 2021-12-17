ae8ea54b958d2a98f6e1a2e744850dc296f7f308
MemeWasTaken <memelazzari02@gmail.com>
2021-12-17 15:41:14 +0100

    help pt.2

--- js/script.js
+++ js/script.js
@@ -5,17 +5,18 @@ const app = new Vue({
       {
         textToDo: 'Lorem ipsum dolor sit amet consectetur adipisicing',
         completed: false,
-      }
+      },
     ]
   },
   methods: {
     checked: function () {
-        if(this.completed == false) {
-            this.completed = true;
-            console.log(this.completed);
+      console.log(this.tasks[i].completed);
+        if(this.tasks[i].completed == false) {
+          this.tasks[i].completed = true;
+            console.log(this.tasks[i].completed);
           } else {
-            this.completed = false;
-            console.log(this.completed);
+            this.tasks[i].completed = false;
+            console.log(this.tasks[i].completed);
         }
     },
   },
